be4965d96fcb57b1f6bb7bcfcb97b35eef3f0727
YosiePW <yosieka_putri_27rpl@student.smktelkom-mlg.sch.id>
2020-08-05 19:06:35 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <First msg="First Component"></First>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import First from './components/First.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    First
   }
 }
 </script>
---
+++ src/components/First.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        color: white;
+        padding: 10px 50px;
+        background-color: #eee;
+    }
+</style>
+
+<script>
+export default {
+    name: "First",
+    props: {
+        msg: String
+    }
+}
+</script>
